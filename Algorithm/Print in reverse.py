27f81fa898633f356d4ddf481c3eb5163d3bfe1e
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-05-04 17:42:51 +0900

    05.04

---
+++ Algorithm/Print in reverse.py
@@ -0,0 +1,30 @@
+
+
+#
+# Complete the 'reversePrint' function below.
+#
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+def reversePrint(llist):
+    if llist is None:
+        return
+    reversePrint(llist.next)
+    print(llist.data)
+
+    
+
