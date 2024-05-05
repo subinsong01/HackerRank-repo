eb6b3772a5138a0908c99d00cda7d76346ff3ef3
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-05-05 19:56:41 +0900

    Difference.py

---
+++ Difference.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    rightSum = 0
+    leftSum = 0
+    
+    for i in range(n):
+        rightSum += arr[i][i]
+        
+    for i in range(n):
+        leftSum += arr[i][n - 1 - i]
+        
+    diff = abs(rightSum - leftSum)
+    
+    return diff
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
